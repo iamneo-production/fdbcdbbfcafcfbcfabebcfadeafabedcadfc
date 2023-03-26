b2b7047cb5b63472c6a5270c7d1b2da1a0451c28
root <root@fdbcdbbfcafcfbcfabebcfadeafabedcadfc-0.fdbcdbbfcafcfbcfabebcfadeafabedcadfc.che.svc.cluster.local>
2023-03-26 18:00:08 +0000

    Backup Branch before deleting

---
+++ puppeteer/test.js
@@ -0,0 +1,148 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('h1');
+    console.log('TESTCASE:testcase1:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase1:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('title');
+    const result1 = await page.evaluate(()=>{
+      let heading = document.querySelector("#benefits");
+      return heading.innerHTML;
+    })
+    const result2 = await page.evaluate(()=>{
+      let heading = document.querySelector("#steps");
+      return heading.innerHTML;
+    })
+    console.log('TESTCASE:testcase2:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase2:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('img');
+    const result1 = await page.evaluate(()=>{
+      let button = document.querySelector("#titleName");
+      return button.innerHTML;
+    })
+    const result2 = await page.evaluate(()=>{
+      let button = document.querySelector("#vegetableSaladImage");
+      return button.innerHTML;
+    })
+    console.log('TESTCASE:testcase3:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase3:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('ul');
+    const result1 = await page.evaluate(()=>{
+      let heading = document.querySelector("#stepForVegetableSalad");
+      return heading.innerHTML;
+    })
+    console.log('TESTCASE:testcase4:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase4:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('p');
+    const result1 = await page.evaluate(()=>{
+      let textBox = document.querySelector("#benefitsParagraph");
+      return textBox.innerHTML;
+    })
+    console.log('TESTCASE:testcase5:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase5:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fdbcdbbfcafcfbcfabebcfadeafabedcadfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('footer');
+    const result1 = await page.evaluate(()=>{
+      let textBox = document.querySelector("#footerText");
+      return textBox.innerHTML;
+    })
+    console.log('TESTCASE:testcase6:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase6:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
